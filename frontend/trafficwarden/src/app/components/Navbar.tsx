cbd710bffdcaaf82cd9616f58cf9a8310346d315
swaraj <swarajmali072004@gmail.com>
2025-04-04 11:50:04 +0530

    frontend started

---
+++ frontend/trafficwarden/src/app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+    <div className=' shadow-xl flex justify-center items-center h-25'>
+<h1 className=' text-3xl'> TrafficWarden</h1>
+    </div>
+    </>
+  )
+}
