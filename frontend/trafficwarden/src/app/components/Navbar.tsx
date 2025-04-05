cbc58e3a308c53336731713af87f50164c8e75c5
swaraj <swarajmali072004@gmail.com>
2025-04-05 15:33:14 +0530

    navbar done

--- frontend/trafficwarden/src/app/components/Navbar.tsx
+++ frontend/trafficwarden/src/app/components/Navbar.tsx
@@ -5,8 +5,10 @@ export default function Navbar() {
   return (
     <>
     <div className=' shadow-xl flex-col justify-evenly items-center  h-26'>
+      <div className=' pl-8 pt-5'>
 <h1 className=' text-3xl'> TrafficWarden</h1>
 <p>A Network Analysis System</p>
+      </div>
     </div>
     </>
   )
